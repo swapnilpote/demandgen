2c5861549e847c23880b960d6e13579dd5990f68
Swapnil Pote <swapnilpote6@gmail.com>
2022-01-14 18:08:33 +0530

    Added logging and person_name validation

--- demandgen_app/app/mod/api/v1/views.py
+++ demandgen_app/app/mod/api/v1/views.py
@@ -15,22 +15,44 @@ def predict():
     text = request.json.get("text")
     callid = request.json.get("callid")
     calldetails = request.json.get("calldetails")
+    logger.debug({"text": text, "callid": callid, "calldetails": calldetails})
+    logger.debug("-"*100)
 
     # Response data
     response = dict()
     response["text"] = text
     response["callid"] = callid
     response["calldetails"] = calldetails
-    response["action"] = dict()
+    response["action"] = list(dict())
 
     result = pred.result(text)
 
-    if result.get("domain")=="dvm" and result.get("entities").get("person_name"):
-        response["action"]["command"] = "hangup"
-        response["action"]["value"] = True
-        response["action"]["identifiedData"] = dict()
-        response["action"]["identifiedData"]["domain"] = "dvm"
-        response["action"]["identifiedData"]["intent"] = ""
-        response["action"]["identifiedData"]["entities"] = result.get("entities")
+    if result.get("entities"):
+        verified_entities = data_validation(calldetails, result.get("entities"))
+
+    if result.get("domain")=="dvm" and result.get("entities") and result.get("entities").get("person_name"):
+        if verified_entities.get("person_name_verified"):
+            response["action"].append({"command": "hangup", "value": True})
+        else:
+            response["action"].append({"command": "hangup", "value": False})
+            response["identifiedData"] = dict()
+            response["identifiedData"]["domain"] = "dvm"
+            response["identifiedData"]["intent"] = ""
+            response["identifiedData"]["entities"] = result.get("entities")
+
+    logger.debug(response)
+    logger.debug("="*100)
 
     return jsonify(response)
+
+
+def data_validation(calldetails: dict, entities: dict):
+    verified_entities = dict()
+    if entities.get("person_name"):
+        person_name = calldetails.get("fname") + " " + calldetails.get("lname")
+        if entities.get("person_name").lower() == person_name.lower():
+            verified_entities["person_name_verified"] = True
+        else:
+            verified_entities["person_name_verified"] = False
+
+    return verified_entities
